cca06271c028b38fe3ac7a0b12082bd9c7a1a331
Saia Fonua <saiafonua@gmail.com>
2022-10-24 09:08:05 -0600

    calculate par and handicap

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 
 <!-- How it was built -->
 
+https://mtec.instructure.com/courses/794448/pages/2-dot-p-2-%7C-golf-score-app-step-by-step-guide?module_item_id=17370973
+https://getbootstrap.com/docs/5.0/content/tables/#responsive-tables
+
 This project was built mobile friendly first
 
 <!-- What does the project do -->
@@ -15,3 +18,4 @@ Bootstrap
 
 <!-- HOw to install and run project -->
 <!-- How to contribute to project  -->
+N
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// let myData = [];
 function getAvailableCourses() {
   return fetch("https://golf-courses-api.herokuapp.com/courses/")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      let courseOptionsHtml = "";
+      let courseOptionsHtml =
+        "<option selected disabled>Select a golf course</option>";
       data.courses.forEach((course) => {
         courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
       });
@@ -19,14 +21,60 @@ function getIndividualCourse(golfId) {
   return fetch(`https://golf-courses-api.herokuapp.com/courses/${golfId}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log("indivisual course data:", data);
-      let teeBoxSelectHtml = "";
-      teeBoxes.forEach(function (teeBox, index) {
-        teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
-          teeBox.totalYards
-        } yards</option>`;
+      console.log(data.data);
+      //Creating table head data
+      const tableElement = document.querySelector("#golf-table");
+      const tHead = document.createElement("thead");
+      const tRow = document.createElement("tr");
+      tableElement.appendChild(tHead);
+      tHead.appendChild(tRow);
+      let thElement = "<th>Hole</th>";
+      let yardageData = "<th>Yardage</th>";
+      let parData = "<th>Par</th>";
+      let handicapData = "<th>Handicap</th>";
+      data.data.holes.forEach(function (hole, index) {
+        thElement += `<th>${hole.hole}</th>`;
+        yardageData += `<td>${hole.teeBoxes[0].yards}</td>`;
+        parData += `<td>${hole.teeBoxes[0].par}</td>`;
+        handicapData += `<td>${hole.teeBoxes[0].hcp}</td>`;
       });
+      tRow.innerHTML = thElement;
 
-      document.getElementById("tee-box-select").innerHTML = teeBoxSelectHtml;
+      //Creating body container rows, header, and data
+      console.log("yardageData", yardageData);
+      const tBody = document.createElement("tbody");
+      tableElement.appendChild(tBody);
+      const tRow2 = document.createElement("tr");
+      const tRow3 = document.createElement("tr");
+      const tRow4 = document.createElement("tr");
+      tBody.appendChild(tRow2);
+      tBody.appendChild(tRow3);
+      tBody.appendChild(tRow4);
+      tRow2.innerHTML = yardageData;
+      tRow3.innerHTML = parData;
+      tRow4.innerHTML = handicapData;
+      // const tBody = document.createElement("tbody");
+      // tableElement.appendChild(tBody);
+
+      // let yards = "<th>Yardage</th>";
     });
+}
+
+// function getTeebox(value) {
+//   let teeBoxSelectHtml = "";
+//   teeBoxes.forEach(function (teeBox, index) {
+//     teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+//       teeBox.totalYards
+//     } yards</option>`;
+//   });
+
+//   document.getElementById("tee-box-select").innerHTML = teeBoxSelectHtml;
+// }
+
+class Player {
+  constructor(name, id = getNextId(), scores = []) {
+    this.name = name;
+    this.id = id;
+    this.scores = scores;
+  }
 }
